0b249c6e3cf181106b4b67d9668527e7af9f2d83
Your Name <you@example.com>
2024-09-28 18:12:59 +0700

    add new comment

--- starter/frontend/src/App.js
+++ starter/frontend/src/App.js
@@ -13,7 +13,7 @@ export default function App() {
 
   return (
     <div className="container">
-      <h1>Movie List</h1>
+      <h1>Movie List All star</h1>
 
       <MovieList onMovieClick={handleMovieClick} />
 
